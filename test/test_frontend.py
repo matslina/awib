ca8c9ab323e71393acde4c662ceca2fef92013ee
matslina <matslina@45282222-9dd6-11dd-93a9-677c3c677ea2>
2009-09-24 09:18:20 +0000

    more unit tests, one of which breaks the current frontend
    git-svn-id: https://awib.googlecode.com/svn/trunk@44 45282222-9dd6-11dd-93a9-677c3c677ea2

--- test/test_frontend.py
+++ test/test_frontend.py
@@ -5,10 +5,10 @@ import ir
 import common
 import awip
 
-frontend_path = "/home/ml/src/awib/trunk/frontend/frontend.b"
+frontend_path = "frontend/frontend.b"
 
-_LANG_C = 1
-_386_LINUX = 2
+_386_LINUX = 1
+_LANG_C = 2
 
 
 class FrontendTest(common.BFTestCase):
@@ -16,14 +16,18 @@ class FrontendTest(common.BFTestCase):
     def setUp(self):
         self.code = open(frontend_path).read()
 
-    def _check_post_condition(self, mem, code):
+    def _check_post_condition(self, mem, code, target=None):
         """Checks frontend post execution memory against expected ir code."""
 
         # sanity
         self.assert_(len(mem) >= 6, "post exec memory must span at least 6 "
                      "cells, only got %d" % len(mem))
-        self.assert_(mem[0] in (_LANG_C, _386_LINUX),
-                     "frontend chose bad target platform T (= %d)" % mem[0])
+        if target is not None:
+            self.assertEquals(mem[0], target,
+                              "bad target platform (%d!=%d)" % (mem[0],target))
+        else:
+            self.assert_(mem[0] in (_LANG_C, _386_LINUX),
+                         "frontend chose bad target platform T (=%d)" % mem[0])
         self.assertEquals(mem[1], 0, "frontend should leave cell 1 blank")
         self.assert_(mem[2], "frontend marked bytecode as not OK")
 
@@ -58,116 +62,169 @@ class FrontendTest(common.BFTestCase):
                 self.fail("expected %r but got %r at op %d" %
                           (expected, op, i))
 
-    def _run_and_check(self, program, ir):
+    ##
+    ## Byte code compilation
+    ##
+
+    def _run_and_check_ir(self, program, ir):
         out, mem = self.run_bf(self.code, program,
                                precondition=[1], steps=5000000)
         self.assertEquals(out, [], "frontend should not produce output")
         self._check_post_condition(mem, ir)
 
     def test_empty_input(self):
-        self._run_and_check("", [])
+        self._run_and_check_ir("", [])
 
     def test_basic_operations(self):
-        self._run_and_check(">+.<-[,]",
-                             [ir.RIGHT(1),
-                              ir.ADD(1),
-                              ir.OUTPUT(),
-                              ir.LEFT(1),
-                              ir.SUB(1),
-                              ir.OPEN(),
-                              ir.INPUT(),
-                              ir.CLOSE()])
-
-    def test_nested_loops(self):
-        self._run_and_check("+[-[+<].]>[[[]+]+]+[+]",
-                             [ir.ADD(1),
-                              ir.OPEN(),
+        self._run_and_check_ir(">+.<-[,]",
+                               [ir.RIGHT(1),
+                                ir.ADD(1),
+                                ir.OUTPUT(),
+                                ir.LEFT(1),
                                 ir.SUB(1),
                                 ir.OPEN(),
-                                  ir.ADD(1),
-                                  ir.LEFT(1),
+                                ir.INPUT(),
+                                ir.CLOSE()])
+
+    def test_nested_loops(self):
+        self._run_and_check_ir("+[-[+<].]>[[[]+]+]+[+]",
+                               [ir.ADD(1),
+                                ir.OPEN(),
+                                  ir.SUB(1),
+                                  ir.OPEN(),
+                                    ir.ADD(1),
+                                    ir.LEFT(1),
+                                  ir.CLOSE(),
+                                  ir.OUTPUT(),
                                 ir.CLOSE(),
-                                ir.OUTPUT(),
-                              ir.CLOSE(),
-                              ir.RIGHT(1),
-                              ir.OPEN(),
+                                ir.RIGHT(1),
                                 ir.OPEN(),
                                   ir.OPEN(),
+                                    ir.OPEN(),
+                                    ir.CLOSE(),
+                                    ir.ADD(1),
                                   ir.CLOSE(),
                                   ir.ADD(1),
                                 ir.CLOSE(),
                                 ir.ADD(1),
-                              ir.CLOSE(),
-                              ir.ADD(1),
-                              ir.OPEN(),
-                                ir.ADD(1),
-                              ir.CLOSE()])
+                                ir.OPEN(),
+                                  ir.ADD(1),
+                                ir.CLOSE()])
 
     def test_clear_loops(self):
-        self._run_and_check("+[-]+[+]",
-                             [ir.ADD(1),
-                              ir.CLEAR(),
-                              ir.ADD(1),
-                              ir.OPEN(),
+        self._run_and_check_ir("+[-]+[+]",
+                               [ir.ADD(1),
+                                ir.CLEAR(),
                                 ir.ADD(1),
-                              ir.CLOSE()])
+                                ir.OPEN(),
+                                  ir.ADD(1),
+                                ir.CLOSE()])
 
     def test_loop_elimination(self):
-        self._run_and_check("[+++]+[][+++].[-][+++]",
-                             [ir.ADD(1),
-                              ir.OPEN(),
-                              ir.CLOSE(),
-                              ir.OUTPUT(),
-                              ir.CLEAR()])
-        self._run_and_check("[-][+++]", [])
+        self._run_and_check_ir("[+++]+[][+++].[-][+++]",
+                               [ir.ADD(1),
+                                ir.OPEN(),
+                                ir.CLOSE(),
+                                ir.OUTPUT(),
+                                ir.CLEAR()])
+        self._run_and_check_ir("[-][+++]", [])
 
     def test_cancellation(self):
-        self._run_and_check("+++-->><-+<-,  <>-++-+-<><>.",
-                             [ir.INPUT(), ir.OUTPUT()])
-        self._run_and_check("-+<>+-><", [])
-        self._run_and_check(".<<<<>>>>++++----.",
-                             [ir.OUTPUT(), ir.OUTPUT()])
+        self._run_and_check_ir("+++-->><-+<-,  <>-++-+-<><>.",
+                               [ir.INPUT(), ir.OUTPUT()])
+        self._run_and_check_ir("-+<>+-><", [])
+        self._run_and_check_ir(".<<<<>>>>++++----.",
+                               [ir.OUTPUT(), ir.OUTPUT()])
 
     def test_contraction(self):
-        self._run_and_check("+++", [ir.ADD(3)])
-        self._run_and_check(['+']*255, [ir.ADD(255)])
-        self._run_and_check(['+']*256, [])
-        self._run_and_check("---", [ir.SUB(3)])
-        self._run_and_check(['-']*255, [ir.SUB(255)])
-        self._run_and_check(['-']*256, [])
-        self._run_and_check("<<<", [ir.LEFT(3)])
-        self._run_and_check(['<']*128, [ir.LEFT(127), ir.LEFT(1)])
-        self._run_and_check(">>>", [ir.RIGHT(3)])
-        self._run_and_check(['>']*128, [ir.RIGHT(127), ir.RIGHT(1)])
+        self._run_and_check_ir("+++", [ir.ADD(3)])
+        self._run_and_check_ir(['+']*255, [ir.ADD(255)])
+        self._run_and_check_ir(['+']*256, [])
+        self._run_and_check_ir("---", [ir.SUB(3)])
+        self._run_and_check_ir(['-']*255, [ir.SUB(255)])
+        self._run_and_check_ir(['-']*256, [])
+        self._run_and_check_ir("<<<", [ir.LEFT(3)])
+        self._run_and_check_ir(['<']*128, [ir.LEFT(127), ir.LEFT(1)])
+        self._run_and_check_ir(">>>", [ir.RIGHT(3)])
+        self._run_and_check_ir(['>']*128, [ir.RIGHT(127), ir.RIGHT(1)])
 
     def test_contraction_and_cancellation(self):
-        self._run_and_check("++++--", [ir.ADD(2)])
-        self._run_and_check("--++++", [ir.ADD(2)])
-        self._run_and_check("++----", [ir.SUB(2)])
-        self._run_and_check("----++", [ir.SUB(2)])
-        self._run_and_check(">>>><<", [ir.RIGHT(2)])
-        self._run_and_check("<<>>>>", [ir.RIGHT(2)])
-        self._run_and_check(">><<<<", [ir.LEFT(2)])
-        self._run_and_check("<<<<>>", [ir.LEFT(2)])
+        self._run_and_check_ir("++++--", [ir.ADD(2)])
+        self._run_and_check_ir("--++++", [ir.ADD(2)])
+        self._run_and_check_ir("++----", [ir.SUB(2)])
+        self._run_and_check_ir("----++", [ir.SUB(2)])
+        self._run_and_check_ir(">>>><<", [ir.RIGHT(2)])
+        self._run_and_check_ir("<<>>>>", [ir.RIGHT(2)])
+        self._run_and_check_ir(">><<<<", [ir.LEFT(2)])
+        self._run_and_check_ir("<<<<>>", [ir.LEFT(2)])
 
-        self._run_and_check(['>']*130 + ['<', '<'],
-                             [ir.RIGHT(127),
-                              ir.RIGHT(1)])
-        self._run_and_check(['>']*130 + ['<', '<', '<'],
-                             [ir.RIGHT(127)])
-        self._run_and_check(['>']*130 + ['<', '<', '<', '<'],
-                             [ir.RIGHT(126)])
+        self._run_and_check_ir(['>']*130 + ['<', '<'],
+                               [ir.RIGHT(127),
+                                ir.RIGHT(1)])
+        self._run_and_check_ir(['>']*130 + ['<', '<', '<'],
+                               [ir.RIGHT(127)])
+        self._run_and_check_ir(['>']*130 + ['<', '<', '<', '<'],
+                               [ir.RIGHT(126)])
 
-        self._run_and_check(['<']*130 + ['>', '>'],
-                             [ir.LEFT(127),
-                              ir.LEFT(1)])
-        self._run_and_check(['<']*130 + ['>', '>', '>'],
-                             [ir.LEFT(127)])
-        self._run_and_check(['<']*130 + ['>', '>', '>', '>'],
-                             [ir.LEFT(126)])
+        self._run_and_check_ir(['<']*130 + ['>', '>'],
+                               [ir.LEFT(127),
+                                ir.LEFT(1)])
+        self._run_and_check_ir(['<']*130 + ['>', '>', '>'],
+                               [ir.LEFT(127)])
+        self._run_and_check_ir(['<']*130 + ['>', '>', '>', '>'],
+                               [ir.LEFT(126)])
 
-    def test_target_selection(self):
-        pass
+
+    ##
+    ## Target string
+    ##
+
+    def _run_and_check_target(self, default_target, target, program, ir=[]):
+        default_target = 17 # some target
+        out, mem = self.run_bf(self.code, program,
+                               precondition=[default_target], steps=500000)
+        self.assertEquals(out, [], "frontend should not produce output")
+        self._check_post_condition(mem, ir, target=target)
+
+    def test_target_empty(self):
+        self._run_and_check_target(17, 17, "")
+        self._run_and_check_target(17, 17, "@")
+        self._run_and_check_target(17, 17, " @")
+        self._run_and_check_target(17, 17, " @ ")
+        self._run_and_check_target(17, 17, "\n")
+        self._run_and_check_target(17, 17, "@\n")
+        self._run_and_check_target(17, 17, " @\n")
+        self._run_and_check_target(17, 17, " @ \n")
+
+    def test_target_incomplete(self):
+        self._run_and_check_target(17, 17, "@lang_")
+        self._run_and_check_target(17, 17, "@lang_ ")
+        self._run_and_check_target(17, 17, "@lang_\n")
+        self._run_and_check_target(17, 17, "@lang_+", ir=[ir.ADD(1)])
+
+        self._run_and_check_target(17, 17, "@386_linu")
+        self._run_and_check_target(17, 17, "@386_linu ")
+        self._run_and_check_target(17, 17, "@386_linu\n")
+        self._run_and_check_target(17, 17, "@386_linu_.", ir=[ir.OUTPUT()])
+
+    def test_target_lang_c(self):
+        self._run_and_check_target(17, _LANG_C, "@lang_c ")
+        self._run_and_check_target(17, _LANG_C, "@lang_c\n")
+        self._run_and_check_target(17, _LANG_C, "@lang_c+", ir=[ir.ADD(1)])
+        self._run_and_check_target(17, _LANG_C, "@lang_c.+",
+                                   ir=[ir.OUTPUT(), ir.ADD(1)])
+        self._run_and_check_target(17, _LANG_C, "@lang_c[.]")
+        self._run_and_check_target(17, _LANG_C, "@lang_c..",
+                                   ir=[ir.OUTPUT(), ir.OUTPUT()])
+
+    def test_target_386_linux(self):
+        self._run_and_check_target(17, _386_LINUX, "@386_linux ")
+        self._run_and_check_target(17, _386_LINUX, "@386_linux,,",
+                                   ir=[ir.INPUT(), ir.INPUT()])
+        self._run_and_check_target(17, _386_LINUX, "@386_linux->",
+                                   ir=[ir.SUB(1), ir.RIGHT(1)])
+        self._run_and_check_target(17, _386_LINUX, "@386_linux[foobar]")
+        self._run_and_check_target(17, _386_LINUX, "@386_linux foobar")
 
     def test_unbalanced_loop(self):
         pass
