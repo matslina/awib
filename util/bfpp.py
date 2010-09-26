d5209caa876b9496416f58d0d4258ce241b7760c
matslina <matslina@45282222-9dd6-11dd-93a9-677c3c677ea2>
2010-09-26 11:11:38 +0000

    added a Tcl interpreter in addition to C and bash
    git-svn-id: https://awib.googlecode.com/svn/trunk@96 45282222-9dd6-11dd-93a9-677c3c677ea2

--- util/bfpp.py
+++ util/bfpp.py
@@ -31,7 +31,124 @@ import optparse
 import os
 
 include_re = re.compile(r'\s*#include\(([^)]+)\)')
+
 BFIS = set([',','.','-','+','[',']','<','>'])
+
+INT_C = ("#define _K_(___,_K,__)(K[_KK]^(___))?_K_^_K_:"
+         "_K _K_ __;\n_K_,_KK,__K,____,__[4>>002<<020];"
+         "_(__){__K=____?__K:__;____=__;while(__K){__K+"
+         "=*(__*__*__+_KK+K)-0133?22^_KK[K+__]^'K'?'.'^"
+         "'u'^'[':-(__[_KK+K]&1):__*__;_KK+=__;_(__);}}"
+         "___(){while(K[++_KK]){    _K_('v'^']',__[,]++"
+         ")_K_(055|___==___,__[,]--)_K_('K'^'u',,++)_K_"
+         "('w'^'K',--,)_K_('_'^4,__[,]?_(_KK[K]&1>>1):_"
+         "(_KK[K]&1))_K_('v'^'+',__[,]?_((_KK[K]&2)-(_K"
+         "K[K]&1)):_(0))K[_KK]-46?_(__[_K_]&1>>1):putch"
+         "ar(__[_K_]);if(_KK[K]^(1<<1^'.'))_(0);else{__"
+         "[_K_]=getchar();}}}main(){--_KK;___();}")
+
+INT_BASH = \
+    (r'O0=($(echo  "$K"|sed -e"s/\(.\)/\\1 /g"));OO'
+     r'=($(for ((i=0;i<65535;i++));do echo "0";done'
+     r'));Oo=0;oo="";oO=0;o0=0;while [ $oO -lt ${#O'
+     r'0[@]} ]; do case ${O0[$oO]} in ">")o0=$((o0+'
+     r'1));;"<")o0=$((o0-1));;"+")OO[$o0]=$(((${OO['
+     r'$o0]}+1)%0x100));;"-")OO[$o0]=$(((${OO[$o0]}'
+     r'+0377)%0x100));;".")echo -ne "\0$(((${OO[$o0'
+     r']}/64)%8))$(((${OO[o0]}/8)%8))$((${OO[o0]}%8'
+     r'))";;",")if ((${#oo}==0)); then oo=$(cat);fi'
+     r';if ((${#oo}>0)); then OO[$o0]=$(printf "%d"'
+      ' "\'${oo:0:1}");oo=${oo:1};fi;;"[")if ((${OO'
+     r'[$o0]}==0));then c=1;while (($c>0));do oO=$('
+     r'($oO+1));if [ ${O0[$oO]} == \[ ];then c=$((c'
+     r'+1));fi;if [ ${O0[$oO]} == \] ]; then ((c--)'
+     r');fi;done;else O0O[$Oo]=$oO;((Oo++));fi;;"]"'
+     r')((Oo--));if ((${OO[$o0]}!=0)); then oO=$(($'
+     r'{O0O[$Oo]}-1));fi;;esac;((oO++));done;')
+
+INT_TCL = """
+fconfigure stdout -encoding binary
+fconfigure stdin -encoding binary
+set p 0
+set ip 0
+set mem {0}
+for {set i 0} {$i<0xffff} {incr i} {lappend mem 0}
+lappend stack 0
+while {$ip < [string length $K]} {
+    switch -exact -- [string index $K $ip] {
+	">" {incr p}
+	"<" {set p [expr $p-1]}
+	"," {
+	    set c [read stdin 1]
+	    if {[string length $c]==1} {
+		lset mem $p [scan $c %c]
+	    }
+	}
+	"." {puts -nonewline stdout [format %c [lindex $mem $p]]}
+	"[" {
+	    if {[lindex $mem $p] == 0} {
+		set depth 1
+		while {$depth != 0} {
+		    incr ip
+		    set c [string index $K $ip]
+		    if {[string equal $c {[} ]} {
+			incr depth
+		    } elseif {[string equal $c {]}]} {
+			set depth [expr $depth-1]
+		    }
+		}
+	    } else {
+		lappend stack [expr $ip - 1]
+	    }
+	}
+	"]" {
+	    if {[lindex $mem $p] != 0} {
+		set ip [lindex $stack end]
+	    }
+	    set stack [lrange $stack 0 end-1]
+	}
+	"+" {lset mem $p [expr ([lindex $mem $p] + 1)%256]}
+	"-" {lset mem $p [expr ([lindex $mem $p] - 1)%256]}
+	default {}
+    }
+    incr ip
+}
+"""
+
+POLYGLOT = """#define EOF char* /* !\\
+cat >>/dev/null <<EOF
+set K {
+[]*/
+EOF
+K="\\
+%(code)s";
+
+#if 0 /*[
+%(bash_interpreter)s
+cat >>/dev/null <<EoF
+*/
+#else
+%(c_interpreter)s
+/*]*/
+#endif
+#define EoF
+#if 0
+[
+}
+%(tcl_interpreter)s
+exit
+]
+#endif
+EoF
+"""
+
+def attach_interpreters(code):
+    data = {'code': ' \\\n'.join(code),
+            'bash_interpreter': INT_BASH,
+            'tcl_interpreter': INT_TCL,
+            'c_interpreter': INT_C}
+
+    return [POLYGLOT % data]
 
 def preprocess(filename):
     """Preprocesses brainfuck sourcecode.
@@ -74,64 +191,6 @@ def format(code, width=78, formatfile=None):
 
     return out + [''.join(code[j:j+width]) for j in xrange(0,len(code),width)]
 
-def attach_interpreter(code):
-    stuff = {}
-
-    # assumes 'char *K' points at code to run
-    stuff['c_interpreter'] =\
-        ("#define _K_(___,_K,__)(K[_KK]^(___))?_K_^_K_:"
-         "_K _K_ __;\n_K_,_KK,__K,____,__[4>>002<<020];"
-         "_(__){__K=____?__K:__;____=__;while(__K){__K+"
-         "=*(__*__*__+_KK+K)-0133?22^_KK[K+__]^'K'?'.'^"
-         "'u'^'[':-(__[_KK+K]&1):__*__;_KK+=__;_(__);}}"
-         "___(){while(K[++_KK]){    _K_('v'^']',__[,]++"
-         ")_K_(055|___==___,__[,]--)_K_('K'^'u',,++)_K_"
-         "('w'^'K',--,)_K_('_'^4,__[,]?_(_KK[K]&1>>1):_"
-         "(_KK[K]&1))_K_('v'^'+',__[,]?_((_KK[K]&2)-(_K"
-         "K[K]&1)):_(0))K[_KK]-46?_(__[_K_]&1>>1):putch"
-         "ar(__[_K_]);if(_KK[K]^(1<<1^'.'))_(0);else{__"
-         "[_K_]=getchar();}}}main(){--_KK;___();}")
-
-    # assumes $KK holds code to run
-    stuff['bash_interpreter'] =\
-        (r'O0=($(echo "$KK"|sed -e"s/\(.\)/\\1 /g"));OO'
-         r'=($(for ((i=0;i<65535;i++));do echo "0";done'
-         r'));Oo=0;oo="";oO=0;o0=0;while [ $oO -lt ${#O'
-         r'0[@]} ]; do case ${O0[$oO]} in ">")o0=$((o0+'
-         r'1));;"<")o0=$((o0-1));;"+")OO[$o0]=$(((${OO['
-         r'$o0]}+1)%0x100));;"-")OO[$o0]=$(((${OO[$o0]}'
-         r'+0377)%0x100));;".")echo -ne "\0$(((${OO[$o0'
-         r']}/64)%8))$(((${OO[o0]}/8)%8))$((${OO[o0]}%8'
-         r'))";;",")if ((${#oo}==0)); then oo=$(cat);fi'
-         r';if ((${#oo}>0)); then OO[$o0]=$(printf "%d"'
-          ' "\'${oo:0:1}");oo=${oo:1};fi;;"[")if ((${OO'
-         r'[$o0]}==0));then c=1;while (($c>0));do oO=$('
-         r'($oO+1));if [ ${O0[$oO]} == \[ ];then c=$((c'
-         r'+1));fi;if [ ${O0[$oO]} == \] ]; then ((c--)'
-         r');fi;done;else O0O[$Oo]=$oO;((Oo++));fi;;"]"'
-         r')((Oo--));if ((${OO[$o0]}!=0)); then oO=$(($'
-         r'{O0O[$Oo]}-1));fi;;esac;((oO++));done;')
-
-    stuff['bf_code'] = ' \\\n'.join(code)
-
-    return ['#define KK char *K\n'
-            '#define true char *K\n'
-            'true;\n'
-            '#define EOF /*\n'
-            'cat>>/dev/null<<EOF\n'
-            '[*/\n'
-            '%(c_interpreter)s\n'
-            '/*]*/\n'
-            'EOF\n'
-            '\n'
-            'KK="\\\n'
-            '%(bf_code)s";\n'
-            '\n'
-            '#if 0/*[\n'
-            '%(bash_interpreter)s\n'
-            '#]*/\n'
-            '#endif\n' % stuff]
-
 def main():
     parser = optparse.OptionParser(usage="%prog [options] FILE")
     parser.add_option("-t", "--format",
@@ -142,7 +201,7 @@ def main():
                       help="write output using line width W (default 78)")
     parser.add_option('', '--interpreter',
                       dest="interpreter", action='store_true',default=False,
-                      help="attach C and bash interpreters")
+                      help="attach C, bash and Tcl interpreters")
     (options, args) = parser.parse_args()
 
     filename = None
@@ -152,7 +211,7 @@ def main():
     code = preprocess(filename)
     code = format(code, options.width, options.format)
     if options.interpreter:
-        code = attach_interpreter(code)
+        code = attach_interpreters(code)
     sys.stdout.write('\n'.join(code))
     sys.stdout.write('\n')
 
